41620f0ef0ddf168c6e3bc66c41bcfec53549d12
t.moechel <t.moechel@softcreation.de>
2019-01-13 07:24:40 +0100

    add simple Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
+const BikeData = [
+  {name: "Garry Fisher"},
+  {name: "Scott"}
+]
 const TMLabel = props => <span> {props.Text} </span> 
-const TMInput = props => <input type= {props.Type} />  
+const TMInput = props => <input type={props.Type} />  
+
+class PropertyList extends Component {
+  render() {
+    return (
+    <div>
+    <ul>
+      {this.props.Bikes.map(bike => <li> {bike.name}</li>) }      
+    </ul>
+  </div>
+  )}
+}
 
 
 class App extends Component {
@@ -14,6 +28,7 @@ class App extends Component {
           <p>
             <TMLabel Text = "Bike name"/>
             <TMInput Type= "Text" />
+            <PropertyList Bikes = {BikeData}/>
           </p>
 
         </header>
